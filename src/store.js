a765843e1e00f11ccdeebca3baf789f4933cf313
Marylene Chan <marylene.chan@gmail.com>
2019-02-19 23:11:51 -0600

    chrome ext

--- src/store.js
+++ src/store.js
@@ -8,7 +8,7 @@ const middleware = [thunk];
 
 // const store = createStore( rootReducer, initialState, compose(applyMiddleware(...middleware),window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION__() || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(rootReducer, initialState, composeEnhancers(
  compose(applyMiddleware(...middleware)))
 
