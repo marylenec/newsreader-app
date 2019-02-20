50051cb893581a182ca93d0c815258e947c0d318
Marylene Chan <marylene.chan@gmail.com>
2019-02-19 19:05:53 -0600

    code spacing

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const Home = (props) => {
+
+return (
+  <main className='ListContainer row'>
+    <section>
+      <h1>Welcome to NewsReader!</h1>
+      <p className='row center-block'>
+      Read our great&nbsp;<Link className='default-link' to='/articles'>articles</Link>&nbsp;or check out&nbsp;<Link className='default-link'  to='/visited'>visited</Link>&nbsp;articles!
+      </p>
+    </section>
+  </main>
+)
+
+}
+export default Home;
